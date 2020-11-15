5bf8ce54d4235d89a75b2863ccd0bf2d91db7713
KHADIR Tayeb <tkhadir@partner.auchan.fr>
2020-11-15 22:23:37 +0100

    :rocket: init

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const http = require('http')
+var cors = require('cors')
 const app = express()
+app.use(cors())
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))

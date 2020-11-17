8088cb21bf54fccedbcd947aa2d5d3c250014bbb
tkhadir <tkhadir.dev@gmail.com>
2020-11-17 17:33:31 +0100

    :rocket: add ssl

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var cors = require('cors')
 const https = require('https')
 const fs = require('fs')
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/libkhadir.fr/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/libkhadir.fr/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/libkhadir.fr/cert.pem')
 }
 const app = express()
